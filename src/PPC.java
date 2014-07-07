cebdcc9d56e486e608e0d7387f862fb0d3413142
Talicska <szolloszs@hotmail.com>
2014-07-07 18:22:40 +0200

    listall() ppc.java

--- src/PPC.java
+++ src/PPC.java
@@ -8,7 +8,7 @@ import java.util.Vector;
 public class PPC {
 
     private static ArrayList<Dye> dyes = new ArrayList<Dye>();
-    private static ArrayList<DyeCylinder> dyecyls = new ArrayList<DyeCylinder>();
+    private static ArrayList<DyeCylinder> dyecylinders = new ArrayList<DyeCylinder>();
     private static Etalon etalonObj;
     private static ArrayList<Lakk> lakks = new ArrayList<Lakk>();
     private static ArrayList<Machine> machines = new ArrayList<Machine>();
@@ -24,7 +24,7 @@ public class PPC {
 
         try {
             dyes = database.getDyes();
-            dyecyls = database.getDyeCylinders();
+            dyecylinders = database.getDyeCylinders();
             etalonMatrix = database.getEtalon();
             lakks = database.getLakks();
             machines = database.getMachine();
@@ -41,8 +41,43 @@ public class PPC {
         return true;
     }
 
-    private void listAll(){
+    private static void listAll(){
 
+        System.out.println("\nDyes");
+        for(int i=0;i<dyes.size();i++){
+            System.out.println(dyes.get(i).getName()+" "+dyes.get(i).getPrice());
+        }
+        System.out.println("\nDyecylinders");
+        for(int i=0;i<dyecylinders.size();i++){
+            System.out.println(dyecylinders.get(i).getVolume()+" "+dyecylinders.get(i).getPercent());
+        }
+        System.out.println("\nEtalon");
+        for (int i = 0; i < etalonObj.getEtalonMatrix().size(); i++) {
+            for (int j = 0; j < etalonObj.getEtalonMatrix().get(i).size(); j++) {
+                System.out.print(etalonObj.getEtalonMatrix().get(i).get(j) + " ");
+            }
+            System.out.print("\n");
+        }
+        System.out.println("\nLakks");
+        for(int i=0;i<lakks.size();i++){
+            System.out.println(lakks.get(i).getName()+" "+lakks.get(i).getPrice());
+        }
+        System.out.println("\nMachines");
+        for(int i=0;i<machines.size();i++){
+            System.out.println(machines.get(i).getName());
+        }
+        System.out.println("\nMagnetcylinders");
+        for(int i=0;i<magnetcylinders.size();i++){
+            System.out.println(magnetcylinders.get(i).getTeeth()+" "+magnetcylinders.get(i).getGirth());
+        }
+        System.out.println("\nMaterials");
+        for(int i=0;i<materials.size();i++){
+            System.out.println(materials.get(i).getName()+" "+materials.get(i).getPrice());
+        }
+        System.out.println("\nMetals");
+        for(int i=0;i<metals.size();i++){
+            System.out.println(metals.get(i).getName()+" "+metals.get(i).getPrice());
+        }
     }
 
     public static void main(String[] args) {
@@ -53,14 +88,9 @@ public class PPC {
             System.exit(1);
         }
 
+        listAll();
 
 
 
-        for (int i = 0; i < etalonObj.getEtalonMatrix().size(); i++) {
-            for (int j = 0; j < etalonObj.getEtalonMatrix().get(i).size(); j++) {
-                System.out.print(etalonObj.getEtalonMatrix().get(i).get(j) + " ");
-            }
-            System.out.print("\n");
-        }
     }
 }
