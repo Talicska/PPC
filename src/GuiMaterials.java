581e97a08b10305d178d174711be0c34a45cfaac
Talicska <szolloszs@hotmail.com>
2014-07-14 15:34:51 +0200

    material manager

--- src/GuiMaterials.java
+++ src/GuiMaterials.java
@@ -14,6 +14,11 @@ public class GuiMaterials extends JFrame implements ActionListener {
     private int width = 600;
     private int height = 400;
 
+    private JTextField textFMatName;
+    private JTextField textFMatPrice;
+
+    private JButton buttonAddMat;
+
     public void actionPerformed(ActionEvent e) {
 
     }
@@ -28,12 +33,8 @@ public class GuiMaterials extends JFrame implements ActionListener {
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         this.setTitle("Alapanyagok");
 
-
         JScrollPane tablePane = new JScrollPane();
         tablePane.setBounds(0,0,320,375);
-
-
-
 
         Vector<String> columnNames = new Vector<String>();
         columnNames.addElement("Név");
@@ -48,6 +49,37 @@ public class GuiMaterials extends JFrame implements ActionListener {
         table.getTableHeader().setFont(new Font("headerFont", Font.BOLD, 12));
         table.getColumnModel().getColumn(0).setPreferredWidth(220);
         table.getColumnModel().getColumn(1).setPreferredWidth(80);
+
+        JLabel labelNewMat = new JLabel("Alapanyag hozzáadása");
+        labelNewMat.setBounds(330,0,200,25);
+        this.add(labelNewMat);
+
+        JLabel labelMatName = new JLabel("Név");
+        labelMatName.setBounds(340,25,30,25);
+        this.add(labelMatName);
+        textFMatName = new JTextField();
+        textFMatName.setBounds(370,27,220,21);
+        this.add(textFMatName);
+
+        JLabel labelMatPrice = new JLabel("Ár");
+        labelMatPrice.setBounds(340,50,30,25);
+        this.add(labelMatPrice);
+        textFMatPrice = new JTextField();
+        textFMatPrice.setBounds(370,52,85,21);
+        this.add(textFMatPrice);
+        JLabel labelEurM = new JLabel("EUR");
+        labelEurM.setBounds(460,52,45,20);
+        this.add(labelEurM);
+
+        buttonAddMat = new JButton("Hozzáad");
+        buttonAddMat.setBounds(505,52,84,21);
+        this.add(buttonAddMat);
+
+
+
+
+
+
 
         tablePane.getViewport().add(table);
         this.add(tablePane);
