07c902936cf9ed4f8ea5b8f2f3d7055d085f7965
Talicska <szolloszs@hotmail.com>
2014-07-14 16:02:21 +0200

    material manager

--- src/GuiMaterials.java
+++ src/GuiMaterials.java
@@ -69,33 +69,33 @@ public class GuiMaterials extends JFrame implements ActionListener {
         table.getColumnModel().getColumn(1).setPreferredWidth(80);
 
         JLabel labelNewMat = new JLabel("Alapanyag hozzáadása");
-        labelNewMat.setBounds(330,0,200,25);
+        labelNewMat.setBounds(330,20,200,25);
         this.add(labelNewMat);
 
         JLabel labelMatName = new JLabel("Név");
-        labelMatName.setBounds(340,25,30,25);
+        labelMatName.setBounds(340,45,30,25);
         this.add(labelMatName);
         textFMatName = new JTextField();
-        textFMatName.setBounds(370,27,220,21);
+        textFMatName.setBounds(370,47,220,21);
         this.add(textFMatName);
 
         JLabel labelMatPrice = new JLabel("Ár");
-        labelMatPrice.setBounds(340,50,30,25);
+        labelMatPrice.setBounds(340,70,30,25);
         this.add(labelMatPrice);
         textFMatPrice = new JTextField();
-        textFMatPrice.setBounds(370,52,85,21);
+        textFMatPrice.setBounds(370,72,85,21);
         this.add(textFMatPrice);
         JLabel labelEurM = new JLabel("EUR");
-        labelEurM.setBounds(460,52,45,20);
+        labelEurM.setBounds(460,72,45,20);
         this.add(labelEurM);
 
         buttonAddMat = new JButton("Hozzáad");
-        buttonAddMat.setBounds(505,52,84,21);
+        buttonAddMat.setBounds(505,72,84,21);
         buttonAddMat.addActionListener(this);
         this.add(buttonAddMat);
 
         buttonDelMat = new JButton("Töröl");
-        buttonDelMat.setBounds(330,300,85,21);
+        buttonDelMat.setBounds(330,330,85,21);
         buttonDelMat.addActionListener(this);
         this.add(buttonDelMat);
 
@@ -121,7 +121,7 @@ public class GuiMaterials extends JFrame implements ActionListener {
                 if (count % 2 == 0) {
                     field.setBackground(flashColor);
                 } else {
-                    field.setBackground(null);
+                    field.setBackground(Color.white);
                     if (count >= totalCount) {
                         ((Timer) evt.getSource()).stop();
                     }
