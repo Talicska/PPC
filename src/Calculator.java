7e1e88d2f3250a2e90665b8ea256d6ca504145ac
posatom <tomposa@hotmail.com>
2014-10-28 14:53:42 +0100

    new searchMagnetCylinder

--- src/Calculator.java
+++ src/Calculator.java
@@ -183,40 +183,53 @@ public class Calculator {
     }*/
 
     private void searchMagnetCylinder(int machineIndex, double height, int magnetCylinderIndex){
-        int pieces=0;
         this.verticalGap=0;
 
         if(magnetCylinderIndex == 0){    // Auto: the program chooses the ideal applicable cylinder
             // to reach the lowest (but bigger or equal than 2) vertical gap
-            double min=machines.get(machineIndex).getCylinders().get(machines.get(machineIndex).getCylinders().size()-1).getGirth();
-            pieces=(int)(machines.get(machineIndex).getCylinders().get(machines.get(machineIndex).getCylinders().size()-1).getGirth() / height);
-            //System.out.println("Kezdeti pieces: "+pieces);
-            boolean found=false;
-            while(pieces > 0 && !found){
-                for(int i=0; i<machines.get(machineIndex).getCylinders().size() && !found; i++){
-                    double rest=machines.get(machineIndex).getCylinders().get(i).getGirth() - pieces * height;
-                    //System.out.println(pieces+" "+rest+" "+rest / pieces+" "+machines.get(machineIndex).getCylinders().get(i).getGirth());
-                    if (rest/pieces >= 2 && rest/pieces < min) {
-                        this.chosenMagnetCylinder=machines.get(machineIndex).getCylinders().get(i);
-                        this.verticalGap=rest/pieces;
-                        found=true;
-                    }
+            int chosenMagnetCylinderIndex = 0;
+            double absoluteMinVerticalGap = machines.get(machineIndex).getCylinders().get(machines.get(machineIndex).getCylinders().size()-1).getGirth();
+            for(int i=0; i<machines.get(machineIndex).getCylinders().size(); i++) {
+                MagnetCylinder cylinder = machines.get(machineIndex).getCylinders().get(i);
+                int maxpieces = (int) (cylinder.getGirth() / height);
+                int pieces = 1;
+                double minVerticalGap = cylinder.getGirth();
+                double rest = cylinder.getGirth() - pieces * height;
+                while (pieces <= maxpieces && rest >= 2 * pieces) {
+                    double verticalGap = rest / pieces;
+                    if (verticalGap < minVerticalGap && verticalGap >= 2)
+                        minVerticalGap = verticalGap;
+                    pieces++;
+                    rest = cylinder.getGirth() - pieces * height;
                 }
-                pieces--;
+                if(minVerticalGap != cylinder.getGirth() && minVerticalGap < absoluteMinVerticalGap) {
+                    absoluteMinVerticalGap = minVerticalGap;
+                    chosenMagnetCylinderIndex = i;
+                }
             }
-            if(found == false){
+            if(absoluteMinVerticalGap == machines.get(machineIndex).getCylinders().get(machines.get(machineIndex).getCylinders().size()-1).getGirth()) {
                 System.out.println("Nem fér bele csak a fele");
+            }else{
+                this.verticalGap = absoluteMinVerticalGap;
+                this.chosenMagnetCylinder = machines.get(machineIndex).getCylinders().get(chosenMagnetCylinderIndex);
             }
         }else{ // we want to use a specific applicable cylinder
             this.chosenMagnetCylinder=machines.get(machineIndex).getCylinders().get(magnetCylinderIndex - 1);
-            pieces=(int)(machines.get(machineIndex).getCylinders().get(magnetCylinderIndex - 1).getGirth() / height);
-            while(pieces > 0 && this.verticalGap < 2){
-                double rest=machines.get(machineIndex).getCylinders().get(magnetCylinderIndex - 1).getGirth() - pieces * height;
-                this.verticalGap=rest/pieces;
-                pieces--;
+            int maxpieces=(int)(chosenMagnetCylinder.getGirth() / height);
+            int pieces=1;
+            double minVerticalGap=chosenMagnetCylinder.getGirth();
+            double rest=chosenMagnetCylinder.getGirth() - pieces*height;
+            while(pieces <= maxpieces && rest >= 2*pieces){
+                double verticalGap=rest/pieces;
+                if(verticalGap < minVerticalGap && verticalGap >= 2)
+                    minVerticalGap=verticalGap;
+                pieces++;
+                rest=chosenMagnetCylinder.getGirth() - pieces*height;
             }
-            if(this.verticalGap == 0){
+            if(minVerticalGap == chosenMagnetCylinder.getGirth()){
                 System.out.println("Nem fér bele csak a fele");
+            }else{
+                this.verticalGap=minVerticalGap;
             }
         }
     }
